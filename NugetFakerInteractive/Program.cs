968cae31571df6cfd85529957606e4d0cf6f35d9
nwkoo <nikitaredkin900@gmail.com>
2022-03-10 10:05:51 +0200

    Add project files.

---
+++ NugetFakerInteractive/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NugetFakerInteractive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int interactive = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < interactive; i++)
+            {
+                Console.WriteLine(Faker.Name.FullName());
+                Console.WriteLine(Faker.Address.StreetAddress());
+                Console.WriteLine(Faker.Identification.DateOfBirth());
+                Console.WriteLine(Faker.Identification.UkNationalInsuranceNumber());
+                Console.WriteLine(Faker.Identification.UkPassportNumber());
+            }
+        }
+    }
+}
